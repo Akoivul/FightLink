80fbad8824850c14eaa1ba3ee419fcc0c75cbae9
aarokoivula <aaro.koivulagit@gmail.com>
2025-03-30 00:59:36 +0200

    Fixed create_item function sql query to include user_id

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ def create_item():
     other_info = request.form["other_info"]
     user_id = session["user_id"]
 
-    sql = """INSERT INTO items (game_name, game_username, availability_start, availability_end, platform, region, other_info)
-             VALUES (?, ?, ?, ?, ?, ?, ?)"""
-    db.execute(sql, [game_name, game_username, availability_start, availability_end, platform, region, other_info])
+    sql = """INSERT INTO items (game_name, game_username, availability_start, availability_end, platform, region, other_info, user_id)
+             VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+    db.execute(sql, [game_name, game_username, availability_start, availability_end, platform, region, other_info, user_id])
     
     return redirect("/")
 
