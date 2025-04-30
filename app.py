461be3b8057331a5d0b62a26583e82ed61564a9c
aarokoivula <aaro.koivulagit@gmail.com>
2025-04-30 17:08:40 +0300

    Add CSRF check to forms

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import secrets
 import sqlite3
 from flask import Flask
 from flask import abort, flash, redirect, render_template, request, session
@@ -10,6 +11,12 @@ app.secret_key = config.secret_key
 
 def require_login():
     if "user_id" not in session:
+        abort(403)
+
+def check_csrf():
+    if "csrf_token" not in request.form:
+        abort(403)
+    if request.form["csrf_token"] != session["csrf_token"]:
         abort(403)
 
 @app.route("/")
@@ -53,6 +60,8 @@ def new_item():
 @app.route("/create_item", methods=["POST"])
 def create_item():
     require_login()
+    check_csrf()
+
     game_name = request.form["game_name"]
     if not game_name or len(game_name) > 100:
         abort(403)
@@ -84,6 +93,7 @@ def create_item():
 @app.route("/sign_up", methods=["POST"])
 def create_signup():
     require_login()
+    check_csrf()
 
     signup = request.form["game_username"]
     if not signup or len(signup) > 50:
@@ -128,6 +138,7 @@ def edit_item(item_id):
 @app.route("/update_item", methods=["POST"])
 def update_item():
     require_login()
+    check_csrf()
 
     item_id = request.form["item_id"]
     item = items.get_item(item_id)
@@ -166,6 +177,7 @@ def update_item():
 @app.route("/remove_item/<int:item_id>", methods=["GET", "POST"])
 def remove_item(item_id):
     require_login()
+
     item = items.get_item(item_id)
     if not item:
         abort(404)
@@ -177,6 +189,7 @@ def remove_item(item_id):
 
     if request.method == "POST":
         if "remove" in request.form:
+            check_csrf()
             items.remove_item(item_id)
             return redirect("/")
         else:
@@ -216,6 +229,7 @@ def login():
         if user_id:
             session["user_id"] = user_id
             session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
             return redirect("/")
         else:
             flash("ERROR: wrong username or password")
